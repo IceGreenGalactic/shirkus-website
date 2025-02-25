ad9e8a2b01aabdb64852a7f7dac2978e718b78c1
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2025-02-25 21:36:08 +0100

    global and theme update

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,27 +2,33 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Playfair+Display:wght@400;700&display=swap');
+
+  :root {
+    --primary-color: #3a3a3a; 
+    --secondary-color: #f7f7f7; 
+    --accent-color: #6a994e; 
+    --header-font: 'Playfair Display', serif; 
+    --text-font: 'Roboto', sans-serif; 
   }
 
   body {
-    font-family: ${(props) => props.theme.fonts.main};
-    background-color: ${(props) => props.theme.colors.background};
-    color: ${(props) => props.theme.colors.text};
-    line-height: 1.6;
-    min-height: 100vh;
+    font-family: var(--text-font);
+    color: var(--primary-color);
+    background-color: var(--secondary-color);
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+  }
+
+  h1, h2, h3 {
+    font-family: var(--header-font);
   }
 
   a {
-    text-decoration: none;
-    color: ${(props) => props.theme.colors.primary};
+    text-decoration: none; 
   }
 
-  header, footer {
-    background-color: ${(props) => props.theme.colors.footer};
-    color: white;
-  }
+ 
+
 `;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,14 +1,21 @@
 // src/styles/theme.js
 export const theme = {
-    colors: {
-      primary: '#6200ea',
-      secondary: '#03dac6',
-      background: '#f5f5f5',
-      text: '#333',
-      footer: '#6200ea',
-    },
-    fonts: {
-      main: 'Arial, sans-serif',
-    },
-  };
-  
+  colors: {
+    primary: "#2C3E50", // Mørk grå
+    secondary: "#60635f", // Lys grå
+    accent: "#5F9EA0", // teal
+    white: "#FFFFFF", // Hvit
+    background: "#F8F9FA", // Lys bakgrunn
+    text: "#33333", // Mørk tekst
+    title: "#E67E22",
+  },
+  fonts: {
+    body: "Roboto, sans-serif",
+    heading: "Playfair Display, serif",
+  },
+  shadows: {
+    boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.1)", 
+    textShadow: "1px 1px 2px rgba(0, 0, 0, 0.6)", 
+  },
+};
+
