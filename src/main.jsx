ec72af75345d43cf2f71d177e8f4f00c08c8407a
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2025-02-25 21:36:32 +0100

    ads theme and global to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App.jsx";
+import { theme } from "./styles/theme.js";
+import { GlobalStyles } from "./styles/GlobalStyles.js";
+import { ThemeProvider } from "styled-components";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
