95c8216537dfa1d0ced591d76963a3e578fdd8d1
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2025-02-26 13:16:55 +0100

    updates vite config and vhanges how sanitClient imports to prossess .env

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,11 +1,10 @@
 import { createClient } from "@sanity/client";
 
-
 const sanityClient = createClient({
-  projectId: import.meta.env.SANITY_PROJECT_ID,
-  dataset: import.meta.env.SANITY_DATASET,
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
   useCdn: true,
-  apiVersion: import.meta.env.SANITY_API_VERSION,
+  apiVersion: process.env.SANITY_API_VERSION,
 });
 
 export default sanityClient;
