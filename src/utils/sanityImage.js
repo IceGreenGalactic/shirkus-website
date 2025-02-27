3975c8507ba7960e8ecab9a8a55cdea91374dd77
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2025-02-27 13:20:16 +0100

    creates sanity image to crop images

--- src/utils/sanityImage.js
+++ src/utils/sanityImage.js
@@ -4,17 +4,16 @@ import sanityClient from "../sanityClient";
 const builder = imageUrlBuilder(sanityClient);
 
 export function urlFor(image) {
-    if (!image?.asset) {
-      console.log("Ingen bilde-asset funnet:", image); 
-      return null;
+    if (!image) {
+        console.log("Ingen bilde-asset funnet:", image);
+        return null;
     }
 
-    
-    if (image.asset.url) {
-      return image.asset.url;
+    // Hvis bildet har en asset, returner URL-en direkte fra asset
+    if (image.asset && image.asset.url) {
+        return image.asset.url;
     }
-  
-    
+
+    // Bruk builder for å generere URL hvis ingen asset URL er tilgjengelig
     return builder.image(image).url();
-  }
-  
+}
