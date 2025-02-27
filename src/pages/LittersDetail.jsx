011c7963f504ff7654e6f6fffc4b35e7fba859a7
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2025-02-27 13:22:30 +0100

    updates littersDetail pages

--- src/pages/LittersDetail.jsx
+++ src/pages/LittersDetail.jsx
@@ -2,21 +2,26 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import sanityClient from "../sanityClient";
 import { urlFor } from "../utils/sanityImage";
+import Modal from "../utils/modal.jsx"; 
 import {
   LitterContainer,
+  ParentInfoContainer,
   ParentInfo,
   ParentImage,
   PuppyGallery,
   PuppyImage,
+  PuppiesContainer,
 } from "./LittersDetail.styled";
 
 const LittersDetail = () => {
   const { id } = useParams();
   const [litter, setLitter] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [selectedImage, setSelectedImage] = useState(null); 
 
   useEffect(() => {
-    sanityClient.fetch(
+    sanityClient
+      .fetch(
         `*[_type == "litter" && _id == $id]{
             mother {
                 name,
@@ -31,11 +36,16 @@ const LittersDetail = () => {
                 info
             },
             puppyDetails,
-            mainImage { asset-> { _id, url }, crop, hotspot }, 
-            additionalImages[]{ asset-> { _id, url }, crop, hotspot }, 
+            mainImage { asset-> { _id, _ref }, crop, hotspot }, 
+            additionalImages[]{ asset-> { _id, _ref }, crop, hotspot }, 
+            galleryImages[] { asset-> { _id, _ref }, crop, hotspot }, 
             textUnderImages,
             dateOfBirth,
-            expectedDateOfBirth
+            expectedDateOfBirth,
+            textUnderMainImage,
+            textUnderGallery,
+            freeText1,
+            freeText2
         }`,
         { id }
       )
@@ -47,7 +57,6 @@ const LittersDetail = () => {
         console.error("Error fetching data:", error);
         setLoading(false);
       });
-      
   }, [id]);
 
   if (loading) {
@@ -58,80 +67,192 @@ const LittersDetail = () => {
     return <div>Fant ingen kull.</div>;
   }
 
+  // Function to render info as bullet points
+  const renderInfoAsBulletPoints = (info) => {
+    return (
+      <ul>
+        {info.split("\n").map((item, index) => (
+          <li className="list-unstyled text-start" key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  // Funksjon for å beregne totalt antall valper
+  const calculateTotalPuppies = () => {
+    if (!litter.puppyDetails || litter.puppyDetails.length === 0) {
+      return 0;
+    }
+
+    return litter.puppyDetails.reduce((total, puppy) => total + puppy.count, 0);
+  };
+
+  const totalPuppies = calculateTotalPuppies();
+
+  const getColorPlural = (color, count) => {
+    switch (color) {
+      case "white":
+        return count > 1 ? "hvite" : "hvit";
+      case "gray":
+        return count > 1 ? "grå" : "grå";
+      case "black":
+        return count > 1 ? "sorte" : "svart";
+      default:
+        return color; 
+    }
+  };
+
+  const getGenderPlural = (gender, count) => {
+    return count > 1
+      ? gender === "male"
+        ? "hanner"
+        : "tisper"
+      : gender === "male"
+      ? "Hann"
+      : "Tispe";
+  };
+
+  const openImageModal = (image) => {
+    setSelectedImage(image);
+  };
+
+  const closeImageModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <LitterContainer className="container">
-      <h2>Kull Detaljer</h2>
-      <div className="row">
-        <div className="col-md-6">
-          <ParentInfo>
-            <h3>
-              Mor: {litter.mother.nickname} ({litter.mother.name})
-            </h3>
+    <LitterContainer className="col-10 m-auto">
+      <h2 className="text-center">Kull Detaljer</h2>
+      <ParentInfoContainer className="m-auto mt-4">
+        <ParentInfo>
+          <h3 className="d-flex flex-column">
+            <strong> Mor: </strong>
+            {litter.mother.name}
+          </h3>
+          <div className="col-12 col-md-8 m-auto">
             {litter.mother.image && (
               <ParentImage
                 src={urlFor(litter.mother.image)}
                 alt={litter.mother.name}
               />
             )}
-            {litter.mother.info && <p>{litter.mother.info}</p>}
-          </ParentInfo>
-        </div>
-        <div className="col-md-6">
-          <ParentInfo>
-            <h3>
-              Far: {litter.father.nickname} ({litter.father.name})
-            </h3>
+            {litter.mother.nickname && <h4>{litter.mother.nickname}</h4>}
+            {litter.mother.info && renderInfoAsBulletPoints(litter.mother.info)}
+          </div>
+        </ParentInfo>
+        <ParentInfo>
+          <h3 className="d-flex flex-column">
+            <strong> Far:</strong> {litter.father.name}
+          </h3>
+          <div className="col-12 col-md-8 m-auto">
             {litter.father.image && (
               <ParentImage
                 src={urlFor(litter.father.image)}
                 alt={litter.father.name}
               />
             )}
-            {litter.father.info && <p>{litter.father.info}</p>}
-          </ParentInfo>
-        </div>
-      </div>
-      {litter.dateOfBirth && (
-        <h4>Dato født: {new Date(litter.dateOfBirth).toLocaleDateString()}</h4>
-      )}
-      {!litter.dateOfBirth && litter.expectedDateOfBirth && (
-        <h4>
-          Forventet fødselsdato:{" "}
-          {new Date(litter.expectedDateOfBirth).toLocaleDateString("no-NO", {
-            year: "numeric",
-            month: "long",
-          })}
-        </h4>
-      )}
-      {litter.mainImage && (
-        <img
-          src={urlFor(litter.mainImage)}
-          alt="Kull bilde"
-        />
-      )}
-      <h4>Detaljer om valper</h4>
-      {litter.puppyDetails &&
-        litter.puppyDetails.map((puppy, index) => (
-          <div key={index}>
-            <h5>Valp {index + 1}:</h5>
-            <p>Kjønn: {puppy.gender === "male" ? "Hann" : "Tispe"}</p>
-            <p>Farge: {puppy.color}</p>
-            <p>Antall: {puppy.count}</p>
+            {litter.father.nickname && <h4>{litter.father.nickname}</h4>}
+            {litter.father.info && renderInfoAsBulletPoints(litter.father.info)}
           </div>
-        ))}
-      {litter.additionalImages && litter.additionalImages.length > 0 && (
-        <PuppyGallery>
-          <h4>Bildegalleri:</h4>
-          {litter.additionalImages.map((image, index) => (
-            <PuppyImage
-              key={index}
-              src={urlFor(image)}
-              alt={`Valp bilde ${index + 1}`}
+        </ParentInfo>
+      </ParentInfoContainer>
+      <PuppiesContainer className="col-10 m-auto">
+        <div>
+          <div className="d-flex align-items-baseline">
+            <h3>Dato Født: </h3>
+            {litter.dateOfBirth && (
+              <h4>{new Date(litter.dateOfBirth).toLocaleDateString()}</h4>
+            )}
+          </div>
+          {!litter.dateOfBirth && litter.expectedDateOfBirth && (
+            <h4>
+              Forventet fødselsdato:{" "}
+              {new Date(litter.expectedDateOfBirth).toLocaleDateString(
+                "no-NO",
+                {
+                  year: "numeric",
+                  month: "long",
+                }
+              )}
+            </h4>
+          )}
+        </div>
+        {litter.mainImage && (
+          <>
+            <img
+              className="mb-5 col-8"
+              src={urlFor(litter.mainImage)}
+              alt="Kull bilde"
             />
-          ))}
-        </PuppyGallery>
+            {litter.textUnderMainImage && <p>{litter.textUnderMainImage}</p>}
+          </>
+        )}
+        <div className="mb-3">
+          {litter.puppyDetails && litter.puppyDetails.length > 0 ? (
+            <>
+              <h4>
+                {new Date(litter.dateOfBirth).toLocaleDateString()} ble det født
+                {totalPuppies} valper:
+              </h4>
+              <h5>
+                {litter.puppyDetails
+                  .reduce((acc, puppy) => {
+                    const gender = puppy.gender;
+                    const color = puppy.color;
+
+                    const existing = acc.find(
+                      (item) => item.color === color && item.gender === gender
+                    );
+                    if (existing) {
+                      existing.count += puppy.count;
+                    } else {
+                      acc.push({ color, gender, count: puppy.count });
+                    }
+                    return acc;
+                  }, [])
+                  .map(
+                    (item) =>
+                      `${item.count} ${getColorPlural(
+                        item.color,
+                        item.count
+                      )} ${getGenderPlural(item.gender, item.count)}`
+                  )
+                  .join(", ")}
+              </h5>
+              <h5> {litter.freeText1 && <p>{litter.freeText1}</p>}</h5>
+            </>
+          ) : null}
+        </div>
+        {/* Vis galleribilder hvis de finnes */}
+        <div className="container mt-5">
+          <h3>Bildegalleri:</h3>
+          {litter.galleryImages && litter.galleryImages.length > 0 && (
+            <PuppyGallery>
+              {litter.galleryImages.map((image, index) => (
+                <PuppyImage
+                  key={index}
+                  src={urlFor(image)}
+                  alt={`Valp bilde ${index + 1}`}
+                  onClick={() => openImageModal(urlFor(image))} 
+                  style={{ cursor: "pointer" }} 
+                />
+              ))}
+            </PuppyGallery>
+          )}{" "}
+          {litter.textUnderGallery && <p>{litter.textUnderGallery}</p>}
+        </div>
+        <div className="container">
+          {/* Vis fritekst om det finnes */}
+          {litter.freeText2 && <p>{litter.freeText2}</p>}
+        </div>
+      </PuppiesContainer>
+
+      {/* Modal for å vise det valgte bildet */}
+      {selectedImage && (
+        <Modal imageUrl={selectedImage} onClose={closeImageModal} />
       )}
-      {litter.textUnderImages && <p>{litter.textUnderImages}</p>}
     </LitterContainer>
   );
 };
--- src/pages/LittersDetail.styled.js
+++ src/pages/LittersDetail.styled.js
@@ -1,18 +1,35 @@
 import styled from "styled-components";
 
 export const LitterContainer = styled.div`
-  max-width: 900px;
   margin: auto;
-  padding: 2rem;
+  h2 {
+    font-size: 2rem;
+    font-family: ${(props) => props.theme.fonts.heading};
+  }
+`;
+
+export const ParentInfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: stretch;
 `;
 
 export const ParentInfo = styled.div`
   text-align: center;
   margin-bottom: 2rem;
+
+  padding: 0.1rem;
+
+  h3 {
+    display: flex;
+    align-items: stretch;
+    min-height: 90px;
+  }
 `;
 
 export const ParentImage = styled.img`
-  max-width: 100%;
+  width: 100%;
   height: auto;
   border-radius: 10px;
   object-fit: cover;
@@ -30,4 +47,14 @@ export const PuppyImage = styled.img`
   height: auto;
   border-radius: 8px;
   object-fit: cover;
+`;
+
+
+export const PuppiesContainer = styled.div`
+
+img{
+
+  border-radius: 8px;
+  object-fit: cover;
+  }
 `;
