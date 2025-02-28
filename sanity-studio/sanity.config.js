3ca24efaeab25d30d2f24e2c2befaf6e0ba270fd
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2025-02-28 14:19:45 +0100

    testing secrets and .env

--- sanity-studio/sanity.config.js
+++ sanity-studio/sanity.config.js
@@ -3,7 +3,6 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
-
 export default defineConfig({
   name: 'default',
   title: 'Shirkus CMS',
