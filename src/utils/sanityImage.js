e81d3d638d0b43acf49282efb8a0fdbc2648a71e
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2025-03-11 14:50:55 +0100

    removes console.log

--- src/utils/sanityImage.js
+++ src/utils/sanityImage.js
@@ -4,15 +4,13 @@ import sanityClient from "../sanityClient";
 const builder = imageUrlBuilder(sanityClient);
 
 export function urlFor(image) {
-    if (!image) {
-        console.log("Ingen bilde-asset funnet:", image);
-        return null;
-    }
+  if (!image) {
+    return null;
+  }
 
-    if (image.asset && image.asset.url) {
-        return image.asset.url;
-    }
+  if (image.asset && image.asset.url) {
+    return image.asset.url;
+  }
 
- 
-    return builder.image(image).url();
+  return builder.image(image).url();
 }
